200eb381412bada6d2c1421a07b6a9dbe5481d42
Dangornushi <dangomushi150@gmail.com>
2024-10-18 15:33:26 +0900

    ADD: インタプリタモードとファイル読み込みモードを区別

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,36 @@ use crate::calc::expr_eval;
 
 use std::io::Write;
 
-fn main() {
-    unsafe { backtrace_on_stack_overflow::enable() };
-    loop {
-        print!("> ");
-        std::io::stdout().flush().unwrap();
+fn interpreter() -> Result<(), String> {
+    // インタプリタモード
+    print!("> ");
+    std::io::stdout().flush().unwrap();
 
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).ok();
-        match expr_eval(&s) {
-            Ok(val) => println!("OK: {:?}", val),
-            _ => println!("構文に誤りがあります"),
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    let value = match expr_eval(&s) {
+        Ok(value) => value,
+        _ => return Err("構文エラー".to_string()),
+    };
+    println!("{:?}", value);
+
+    Ok(())
+    // -----------------
+}
+
+fn main() {
+    // コマンド引数を取得
+    let args: Vec<String> = std::env::args().collect();
+    let mode = args[1].as_str();
+
+    match mode {
+        "-i" => loop {
+            interpreter();
+        },
+        _ => {
+            println!("ファイル内実行モード");
         }
     }
+    // スタックオーバーフローの際の原因特定
+    unsafe { backtrace_on_stack_overflow::enable() };
 }
-// Ok(val) => println!("{}", val),
